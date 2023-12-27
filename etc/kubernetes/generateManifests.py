16b83a9ffda864cf8787564d758f386cef26af58
cyu72 <cyu72@binghamton.edu>
2023-12-27 00:54:14 -0500

    added network policies in manifest generation file

--- etc/kubernetes/generateManifests.py
+++ etc/kubernetes/generateManifests.py
@@ -1,3 +1,5 @@
+import random
+
 droneNum = int(input("Total number of drones in swarm: "))
 imagePullLocation = int(input("1): DockerHub\n2): Local\nImage pull location: "))
 if imagePullLocation == 1:
@@ -21,7 +23,6 @@ metadata:
     tier: drone
 spec:
   hostname: drone{num}
-  dnsPolicy: ClusterFirst
   containers: 
     - name: drone{num}
       image: {droneImage}
@@ -65,7 +66,6 @@ metadata:
     tier: drone
 spec:
   hostname: gcs
-  dnsPolicy: ClusterFirst
   containers: 
     - name: gcs
       image: {gcsImage}
@@ -83,6 +83,7 @@ metadata:
   name: drone-broadcast-service
   namespace: default
 spec:
+  clusterIP: None
   selector:
     tier: drone
   ports:
@@ -93,3 +94,79 @@ spec:
     file.write(delim)
     file.write(broadcastService)
     file.close()
+
+# Create a matrix that is of variable size
+# Randomly place each drone in the matrix, where each drone is represented by its number
+
+def generate_random_matrix(n, numDrones):
+  matrix = [[0] * n for _ in range(n)]
+  drone_numbers = random.sample(range(1, numDrones + 1), numDrones)
+  
+  for num in drone_numbers:
+    while True:
+      row = random.randint(0, n - 1)
+      col = random.randint(0, n - 1)
+      if matrix[row][col] == 0:
+        matrix[row][col] = num
+        break
+  
+  return matrix
+
+n = 9  # size of the matrix
+matrix = generate_random_matrix(n, droneNum)
+
+for row in matrix:
+  for element in row:
+    print(element, end=' ')
+  print()
+
+with open('deploymentNetworkPolicy.yml', 'w') as file:
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] != 0:
+                neighbors = []
+                if i > 0 and matrix[i-1][j] != 0: # Only 4 tiles around
+                    neighbors.append(matrix[i-1][j])
+                if i < len(matrix)-1 and matrix[i+1][j] != 0:
+                    neighbors.append(matrix[i+1][j])
+                if j > 0 and matrix[i][j-1] != 0:
+                    neighbors.append(matrix[i][j-1])
+                if j < len(matrix[i])-1 and matrix[i][j+1] != 0:
+                    neighbors.append(matrix[i][j+1])
+
+                print(f"Neighbors of drone{matrix[i][j]}: {neighbors}")
+                if neighbors:
+                  networkPolicy = f"""apiVersion: networking.k8s.io/v1
+kind: NetworkPolicy
+metadata:
+  name: ingress-selector{matrix[i][j]}
+spec:
+  podSelector:
+    matchLabels:
+      app: drone{matrix[i][j]}
+      tier: drone
+  ingress:
+  - from:
+    - podSelector:
+        matchExpressions:
+        - {{key: app, operator: In, values: [gcs, {', '.join(['drone' + str(neighbor) for neighbor in neighbors])}]}}
+        - {{key: tier, operator: In, values: [drone]}}"""
+                  file.write(networkPolicy + "\n" + delim)
+                else:
+                    networkPolicy = f"""apiVersion: networking.k8s.io/v1
+kind: NetworkPolicy
+metadata:
+  name: ingress-selector{matrix[i][j]}
+spec:
+  podSelector:
+    matchLabels:
+      app: drone{matrix[i][j]}
+      tier: drone
+  ingress:
+  - from:
+    - podSelector:
+        matchLabels:
+          app: gcs"""
+                    file.write(networkPolicy + "\n" + delim)
+
+file.close()
