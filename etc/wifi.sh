bf9ccc0dd7700a6f8c6d6cc8980d2c40b351e69c
cyu72 <cyu72@binghamton.edu>
2024-12-05 21:21:48 -0500

    modify wifi restoration script

--- etc/wifi.sh
+++ etc/wifi.sh
@@ -5,21 +5,9 @@ echo "Stopping ad hoc network configuration..."
 sudo ifconfig wlan0 down
 sudo systemctl start wpa_supplicant.service
 
-echo "Backing up ad hoc configuration file..."
-if [ -f /etc/network/interfaces.d/adhoc.conf ]; then
-    sudo mv /etc/network/interfaces.d/adhoc.conf /etc/network/interfaces.d/adhoc.conf.backup
-    echo "Ad hoc configuration backed up"
-else
-    echo "No ad hoc configuration file found"
-fi
+sudo rm /etc/network/interfaces
 
 echo "Stopping DHCP service if running..."
-if systemctl is-active --quiet dnsmasq; then
-    sudo systemctl stop dnsmasq
-    echo "DHCP service stopped"
-else
-    echo "DHCP service not running"
-fi
 
 echo "Restoring normal WiFi configuration..."
 sudo wpa_cli reconfigure
